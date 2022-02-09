8328788d4d5f92720b397796933d4fe9d6585169
datacreator001 <amirrorphone@gmail.com>
2022-02-08 21:05:32 -0500

    created courseAction and Reducer.... and Created Store

--- personal_projects/chapter-2-redux/lecture_1/03/demos/before/src/components/courses/CoursesPage.js
+++ personal_projects/chapter-2-redux/lecture_1/03/demos/before/src/components/courses/CoursesPage.js
@@ -11,9 +11,14 @@ class CoursesPage extends Component {
 		const course = { ...this.state.course, title: event.target.value };
 		this.setState({ course });
 	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		alert(this.state.course.title);
+	};
 	render() {
 		return (
-			<form action=''>
+			<form onSubmit={this.handleSubmit}>
 				<h2>Courses</h2>
 				<h3>Add Course</h3>
 				<input
