3fbc812de4ad5f6e6000ba96e5ebd4ebd61381f9
datacreator001 <amirrorphone@gmail.com>
2022-02-13 19:34:27 -0500

    binded actions to bindAlllActions

--- personal_projects/chapter-2-redux/lecture_1/03/demos/before/src/components/courses/CoursesPage.js
+++ personal_projects/chapter-2-redux/lecture_1/03/demos/before/src/components/courses/CoursesPage.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import * as courseActions from "../../redux/actions/courseActions" //using courseActions from "../../redux/actions/courseActions"
-import PropTypes from "prop-types"
+import * as courseActions from '../../redux/actions/courseActions'; //using courseActions from "../../redux/actions/courseActions"
+import PropTypes from 'prop-types';
+import {bindActionCreators} from 'redux'
 class CoursesPage extends Component {
 	state = {
 		course: {
 			title: '',
 		},
 	};
-//Arrow functions auto binds and recognizes the class instance
+	//Arrow functions auto binds and recognizes the class instance
 	handleChange = (event) => {
 		const course = { ...this.state.course, title: event.target.value };
 		this.setState({ course });
 	};
-// You have to dispatch an action. Action creators just return an object
+	// You have to dispatch an action. Action creators just return an object
 	handleSubmit = (e) => {
 		e.preventDefault();
-		this.props.dispatch(courseActions.createCourse(this.state.course)) // since im using courseActions I can use the createCourse action creator
+		this.props.actions.createCourse(this.state.course) // no need to call dispatch since its being handled in mapDispatchToProps now
 		
+		// this.props.dispatch(courseActions.createCourse(this.state.course)); // since im using courseActions I can use the createCourse action creator
 	};
 	render() {
 		return (
@@ -32,7 +34,7 @@ class CoursesPage extends Component {
 					value={this.state.course.title}
 				/>
 				<input type='submit' value='Save' />
-				{this.props.courses.map(course =>(
+				{this.props.courses.map((course) => (
 					<div key={course.title}>{course.title}</div> // Keys help React track each array element
 				))}
 			</form>
@@ -42,8 +44,8 @@ class CoursesPage extends Component {
 
 CoursesPage.propTypes = {
 	courses: PropTypes.array.isRequired,
-	dispatch : PropTypes.func.isRequired,
-}
+	actions: PropTypes.object.isRequired, //set actions in propTypes and expects a object
+};
 
 function mapStateToProps(state) {
 	return {
@@ -51,4 +53,10 @@ function mapStateToProps(state) {
 	};
 }
 
-export default connect(mapStateToProps)(CoursesPage);  //connect auto passes dispatch in if we omit mapDispatchToProps here
+function mapDispatchToProps(dispatch) { // determines what actions are available on props in our component
+	return{
+		actions: bindActionCreators(courseActions, dispatch), // wraps all of course actions in a call to dispatch // Also return a mimic object
+	}
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CoursesPage); //connect auto passes dispatch in if we omit mapDispatchToProps here
